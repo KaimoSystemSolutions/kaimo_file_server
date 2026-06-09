6c0648e3dc8e16ae513bb3662ece9ad3f38f2e96
MaxBoss <schnurritv2@gmail.com>
2026-06-09 03:49:55 +0200

    Replaced SVGs in FileBrowser.razor and MainLayout.razor

--- src/Kaimo_File_Server.Web/DynamicHelpers/SvgOptions.cs
+++ src/Kaimo_File_Server.Web/DynamicHelpers/SvgOptions.cs
@@ -1,3 +1,4 @@
 namespace Kaimo_File_Server.Web.DynamicHelpers;
 
-public record SvgOptions(int Width = 18, int Height = 18, string Color = "currentColor", float StrokeWidth = 1.5f, string Fill = "none") {}
+public record SvgOptions(int Width = 18, int Height = 18, string Color = "currentColor", 
+    float StrokeWidth = 1.5f, string Fill = "none", string? CssClass = null, string? Style = null) {}
--- src/Kaimo_File_Server.Web/Services/AssetProvider.cs
+++ src/Kaimo_File_Server.Web/Services/AssetProvider.cs
@@ -8,9 +8,15 @@ public class AssetProvider
 {
     
     private static readonly string SVG_HEADER =
-        "<svg width=\"{width}\" height=\"{height}\" viewBox=\"0 0 24 24\" fill=\"{fill}\"\n     stroke=\"{color}\" stroke-width=\"{stroke-width}\"\n     xmlns=\"http://www.w3.org/2000/svg\">";
+        "<svg width=\"{width}\" height=\"{height}\" viewBox=\"0 0 24 24\" fill=\"{fill}\"\n {class} {style} stroke=\"{color}\" stroke-width=\"{stroke-width}\"\n     xmlns=\"http://www.w3.org/2000/svg\">";
 
     private static readonly string SVG_FOOTER = "</svg>";
+
+    private static readonly HashSet<string> RELEVANT_SVG_TAGS = new()
+    {
+        "<path",
+        "<line"
+    };
     
     private readonly IWebHostEnvironment _env;
     private readonly Dictionary<string, string> _pathsCache = new();
@@ -32,7 +38,7 @@ public class AssetProvider
 
     private string loadSvgPaths(string relativePath)
     {
-        if (_pathsCache.TryGetValue(relativePath, out var svgPaths))
+        if (_pathsCache.TryGetValue(relativePath, out var svgPaths) && !_env.IsDevelopment())
             return svgPaths;
         
         var fullPath = Path.Combine(_env.WebRootPath, relativePath);
@@ -40,7 +46,18 @@ public class AssetProvider
         
         // remove everything that is not a svg path
         var lines = rawSVG.Split("\n");
-        svgPaths = string.Join('\n', lines.Where(l => l.Trim().StartsWith("<path")));
+        svgPaths = string.Join('\n', lines.Where(l =>
+        {
+            string trimmedLine = l.Trim();
+            
+            foreach (var tag in RELEVANT_SVG_TAGS)
+            {
+                if (trimmedLine.StartsWith(tag))
+                    return true;
+            }
+
+            return false;
+        }));
         
         _pathsCache[relativePath] = svgPaths;
         return svgPaths;
@@ -55,7 +72,10 @@ public class AssetProvider
                 .Replace("{fill}", options.Fill)
                 .Replace("{color}", options.Color)
                 .Replace("{stroke-width}", options.StrokeWidth.ToString("0.0"))
+                .Replace("{style}", options.Style is null ? "" : "style=\"" + options.Style +"\"")
+                .Replace("{class}", options.CssClass is null ? "" : "class=\"" + options.CssClass +"\"")
             ;
+        
         
         return header + svgPaths + SVG_FOOTER;
     }
